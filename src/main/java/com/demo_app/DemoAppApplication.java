a325ba8a8fc77f078a570db1215c12af3fa4ed1e
Amit Kumar <91861953+Amitcodingninja@users.noreply.github.com>
2025-08-01 22:42:46 +0530

    Update DemoAppApplication.java

--- src/main/java/com/demo_app/DemoAppApplication.java
+++ src/main/java/com/demo_app/DemoAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoAppApplication {
 //amit
+	//kriti
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
 	}
